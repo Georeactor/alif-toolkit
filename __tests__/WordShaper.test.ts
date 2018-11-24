1997b2182d219ad0e91ff02123cc8cec94728f7a
Nick Doiron <ndoiron@mapmeld.com>
2018-11-23 23:39:49 -0500

    test isolated form in WordShaper

--- __tests__/WordShaper.test.ts
+++ __tests__/WordShaper.test.ts
@@ -4,3 +4,8 @@ test('Return initial, medial, and final versions of a letter written 3x', () => 
   let val = WordShaper("\u062C\u062C\u062C");
   expect(val).toEqual("\uFE9F\uFEA0\uFE9E");
 });
+
+test('Return isolated version of a letter written 1x', () => {
+  let val = WordShaper("\u062C");
+  expect(val).toEqual("\uFE9D");
+});
