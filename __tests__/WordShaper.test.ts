33f28dd54a2e5bfbe40f76da6ba11664b99c37ac
Nick Doiron <ndoiron@mapmeld.com>
2018-12-02 15:48:37 -0500

    LA fixes in WordShaper

--- __tests__/WordShaper.test.ts
+++ __tests__/WordShaper.test.ts
@@ -9,3 +9,18 @@ test('Return isolated version of a letter written 1x', () => {
   let val = WordShaper("\u062C");
   expect(val).toEqual("\uFE9D");
 });
+
+test('LA by itself', () => {
+  let val = WordShaper("لا");
+  expect(val).toEqual("\uFEFB");
+});
+
+test('LA at start of word', () => {
+  let val = WordShaper("لاو");
+  expect(val).toEqual("\uFEFB\uFEED");
+});
+
+test('LA at end of word', () => {
+  let val = WordShaper("آهنغركلا");
+  expect(val[val.length - 1]).toEqual("\uFEFC")
+});
--- src/WordShaper.ts
+++ src/WordShaper.ts
@@ -1,11 +1,23 @@
 import { isArabic } from './isArabic';
-import { tashkeel, lineBreakers } from './reference';
+import { tashkeel, lineBreakers, lams, alefs } from './reference';
 import { CharShaper } from './CharShaper';
+import ligatureReference from './unicode-ligatures';
 
 export function WordShaper (word: string) {
   let state = 'initial';
   let output = '';
   for (let w = 0; w < word.length; w++) {
+    let nextLetter = ' ';
+    for (let nxw = w + 1; nxw < word.length; nxw++) {
+      if (!isArabic(word[nxw])) {
+        break;
+      }
+      if (tashkeel.indexOf(word[nxw]) === -1) {
+        nextLetter = word[nxw];
+        break;
+      }
+    }
+
     if (!isArabic(word[w])) {
       // space or other non-Arabic
       output += word[w];
@@ -13,11 +25,17 @@ export function WordShaper (word: string) {
     } else if (tashkeel.indexOf(word[w]) > -1) {
       // tashkeel - add without changing state
       output += word[w];
-    } else if ((w === word.length - 1) // last letter
-      || (w < word.length - 1 && !isArabic(word[w + 1])) // next letter is space or other non-Arabic
-      || (lineBreakers.indexOf(word[w]) > -1)) { // this letter is known to break lines
+    } else if ((nextLetter === ' ') // last Arabic letter in this word
+      || (lineBreakers.indexOf(word[w]) > -1)) { // the current letter is known to break lines
 
       output += CharShaper(word[w], state === 'initial' ? 'isolated' : 'final');
+      state = 'initial';
+    } else if (lams.indexOf(word[w]) > -1 && alefs.indexOf(nextLetter) > -1) {
+      // LA letters - advance an additional letter after this
+      output += ligatureReference[word[w] + nextLetter][(state === 'initial' ? 'isolated' : 'final')];
+      while (word[w] !== nextLetter) {
+        w++;
+      }
       state = 'initial';
     } else {
       output += CharShaper(word[w], state);
